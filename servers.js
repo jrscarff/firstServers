d998598ca408c7ddb31cd56d2e2d8df41d2d8ee4
Ryan Scarff <williamscarff@calvisitor-10-105-155-47.calvisitor.1918.berkeley.edu>
2018-02-23 10:43:49 -0800

    random good and bad phrases now when refreshed

--- servers.js
+++ servers.js
@@ -3,13 +3,17 @@ var http = require("http");
 var PORT1 = 7000;
 var PORT2 = 7500;
 
+var goodArray = ["You look great today dawg!", "Have you been hitting the gym?", "You're doing amazing.", "Keep it up!", "You Smart, You Loyal.", "All your sports teams will win today.", "You're a genius.", "Love ya dawg.", "You have a beautiful soul."];
+var badArray = ["You look ugly dawg.", "You look even worse today", "When's the last time you showered?", "You suck", "None of these are good I don't know why you keep reloading, you must be a negative person.", "Do you even know what fashion is?", "You really need to clean up dawg.", "Your breath stinks."];
+
 function goodRequest(request, response) {
 
-	response.end("You look good today!");
+	console.log(request.url, request.method);
+	response.end(goodArray[Math.floor(Math.random() * goodArray.length)]);
 }
 
 function badRequest(request, response) {
-	response.end("You look ugly dawg.");
+	response.end(badArray[Math.floor(Math.random() * badArray.length)]);
 }
 
 var server1 = http.createServer(goodRequest);
